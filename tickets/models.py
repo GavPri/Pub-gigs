0224422e18565263ba508d58e014b1438858a69c
GavPri <gavinprior5@gmail.com>
2023-07-22 22:44:53 +0000

    update gig model

--- tickets/models.py
+++ tickets/models.py
@@ -3,3 +3,11 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
+class Gig(models.Model):
+    band = models.CharField(max_length=50)
+    time = models.TimeField()
+    venue = models.CharField(max_length=200)
+    date = models.DateField()
+
+    def __str_(self):
+        return f"{self.band} @ {self.venue}"
