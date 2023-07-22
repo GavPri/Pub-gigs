4211bb364f2e1e99337549066cc175d8ec740d78
GavPri <gavinprior5@gmail.com>
2023-07-22 23:28:11 +0000

    create base hhtml

--- tickets/views.py
+++ tickets/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Gig
 
+
 class GigList(generic.ListView):
     model = Gig
     queryset = Gig.objects.all
     template_name = 'index.html'
     paginate_by = 6
-
