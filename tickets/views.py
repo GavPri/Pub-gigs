4b60b7806ff211e581ea92a12b21e631f67364ca
GavPri <gavinprior5@gmail.com>
2023-07-22 23:26:29 +0000

    create GigList view

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Gig
+
+class GigList(generic.ListView):
+    model = Gig
+    queryset = Gig.objects.all
+    template_name = 'index.html'
+    paginate_by = 6
+
