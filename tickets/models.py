08e78179758cd52e2de732c9e43dcf64df5c5691
GavPri <gavinprior5@gmail.com>
2023-07-22 22:53:52 +0000

    migrate

--- tickets/models.py
+++ tickets/models.py
@@ -8,6 +8,7 @@ class Gig(models.Model):
     time = models.TimeField()
     venue = models.CharField(max_length=200)
     date = models.DateField()
+    featured_image = CloudinaryField('image', default='placeholder')
 
     def __str_(self):
         return f"{self.band} @ {self.venue}"
