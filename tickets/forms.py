a7889c9ccc31c3ff97e9edc0142b556d37d803b7
GavPri <gavinprior5@gmail.com>
2023-07-25 21:16:29 +0000

    remove form

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,10 +1,3 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Reservation
 
-
-# Reservation Form
-class ReservationForm(ModelForm):
-    class Meta:
-        model = Reservation
-        fields = "__all__"
