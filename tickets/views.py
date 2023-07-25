26e61c496917145126db0917454fe84baf4f442e
GavPri <gavinprior5@gmail.com>
2023-07-25 22:55:37 +0000

    url path for reservations

--- tickets/views.py
+++ tickets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Gig
+from .forms import BookingForm
 
 
 class GigList(generic.ListView):
@@ -8,3 +9,8 @@ class GigList(generic.ListView):
     queryset = Gig.objects.all()
     template_name = 'index.html'
     paginate_by = 6
+
+
+# Booking
+def add_booking(request):
+    return render(request, 'reservations.html', {})
