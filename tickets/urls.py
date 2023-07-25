c11dee3d8898776f2de75fa2b4f5036f8d56330e
GavPri <gavinprior5@gmail.com>
2023-07-25 21:11:55 +0000

    remove all reservation parts

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,5 +4,4 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.GigList.as_view(), name='home'),
-    path('reservations/', views.ReservationList.as_view(), name='reservations')
 ]
